8f6819d179c8e66404c7bd67c9d27f7f5589f728
Manoel Neto <manoel.neto.morais@gmail.com>
2020-05-13 10:31:16 -0300

    Retirando techo de código com erro

--- gest_clientes/urls.py
+++ gest_clientes/urls.py
@@ -9,7 +9,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include(urls_clientes)),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout')
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
